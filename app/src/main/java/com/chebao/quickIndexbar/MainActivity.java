609be4410576fc23a2511b5d0fd076e9eb288391
mytime <cmba@qq.com>
2016-08-30 15:17:12 +0800

    快速索引工具完成

--- app/src/main/java/com/chebao/quickIndexbar/MainActivity.java
+++ app/src/main/java/com/chebao/quickIndexbar/MainActivity.java
@@ -2,6 +2,9 @@ package com.chebao.quickIndexbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * 快速索引
@@ -10,9 +13,17 @@ import android.os.Bundle;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private QuickIndexBar quickIndexBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quickIndexBar = (QuickIndexBar) findViewById(R.id.quickIndexBar);
+        quickIndexBar.setOnTouchLetterListener(new QuickIndexBar.OnTouchLetterLintener() {
+            @Override
+            public void onToucheLetter(String letter) {
+                Log.e("tag","letter:"+ letter);
+            }
+        });
     }
 }
--- app/src/main/java/com/chebao/quickIndexbar/QuickIndexBar.java
+++ app/src/main/java/com/chebao/quickIndexbar/QuickIndexBar.java
@@ -6,22 +6,27 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
- *
  * Created by iwan on 16/8/30.
  * 快速索引工具类
- *  1.重写三个构造方法
- *  2.定义26字母数组
- *  3.在view上绘制26个字母，首先要初始一个画笔
- *  4.获取视图宽度
- *      绘制文本的x坐标: width/2
- *      绘制文本的Y坐标: 格子高度的一半 + 文字高度的一半 + position * 格子高度
+ * <p/>
+ * 1.重写三个构造方法
+ * 2.定义26字母数组
+ * 3.在view上绘制26个字母，首先要初始一个画笔 ，重写onDraw方法
+ * 4.获取视图宽高 重写onSizeChanged方法
+ * 5.绘制文本的x坐标: width/2
+ * 绘制文本的Y坐标: 格子高度的一半 + 文字高度的一半 + position * 格子高度
+ * 计算触摸点对应的字母：根据触摸点的y坐标除以cellHeight,得到的值就是字母对应的索引
+ * <p/>
+ * 6. 获取触摸的字母 重写onTouchEvent()
  */
-public class QuickIndexBar extends View{
+public class QuickIndexBar extends View {
     //定义26字母数组
-    private String[] indexArr = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    private String[] indexArr = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
     //定义一个画笔
     private Paint paint;
@@ -35,11 +40,13 @@ public class QuickIndexBar extends View{
         super(context);
         init();
     }
+
     //重写
     public QuickIndexBar(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
     }
+
     //重写
     public QuickIndexBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
@@ -49,10 +56,10 @@ public class QuickIndexBar extends View{
     /**
      * 初始化方法
      */
-    private void init(){
+    private void init() {
         paint = new Paint(Paint.ANTI_ALIAS_FLAG); // 初始化画笔，设置抗锯齿，
         paint.setColor(Color.WHITE); // 画笔颜色
-        paint.setTextSize(18); // 设置字号16sp
+        paint.setTextSize(30); // 设置字号16sp
         paint.setTextAlign(Paint.Align.CENTER); //设置显示文字的起点是文字边框的底边的中心（显示文字总长度的一半）
 
     }
@@ -64,7 +71,7 @@ public class QuickIndexBar extends View{
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         width = getMeasuredWidth(); // 视图宽度
-        cellHeight = getMeasuredHeight()*1f/indexArr.length ;// 每个格子的高度（ 等分26份）
+        cellHeight = getMeasuredHeight() * 1f / indexArr.length;// 每个格子的高度（ 等分26份）
     }
 
     //在view上绘制26个字母，
@@ -79,17 +86,78 @@ public class QuickIndexBar extends View{
 
         for (int i = 0; i < indexArr.length; i++) {
 
-            float x = width/2;
-            float y = cellHeight/2 + getTextHeight(indexArr[i])/2 + i*cellHeight;
+            float x = width / 2;
+            float y = cellHeight / 2 + getTextHeight(indexArr[i]) / 2 + i * cellHeight;
 
-            canvas.drawText(indexArr[i],x,y,paint);
+            // 点击字母时重绘26个字母，被点击的字母绘制成黑色
+            paint.setColor(lastIndex == i?Color.BLACK:Color.WHITE);
+
+            canvas.drawText(indexArr[i], x, y, paint);
         }
 
     }
+
+    private int lastIndex = -1; // 记录上次触摸字母的索引值
+
+    /**
+     * 获取触摸的文字
+     *
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:           //按下和移动执行同一个代码
+                float y = event.getY();             //触摸点的Y坐标
+                int index = (int) (y / cellHeight); //触摸点的Y坐标 除以 格子高度，得到索引值
+                if (lastIndex != index) {           //记录值和当前值不相等才打印
+                    //Log.e("tag", indexArr[index]);  //显示字母
+
+                    if (index >= 0 && index < indexArr.length) { // 合法的值（精益求精）
+                        //回调字母
+                        if (listener != null) {
+                            listener.onToucheLetter(indexArr[index]);
+                        }
+                    }
+
+                }
+                lastIndex = index;
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+                lastIndex = -1;                     //重置
+
+                break;
+
+        }
+        //按下和拖动时字母颜色变化
+        //引起重绘(在这里回调onDraw()方法)
+        invalidate();
+
+        return true;
+    }
+
     // 获取文本的高度
     private int getTextHeight(String text) {
         Rect bounds = new Rect();// 矩形对象
-        paint.getTextBounds(text,0, text.length(),bounds); // 获得文本边界
+        paint.getTextBounds(text, 0, text.length(), bounds); // 获得文本边界
         return bounds.height(); // 高
     }
+
+    private OnTouchLetterLintener listener;
+
+    public void setOnTouchLetterListener(OnTouchLetterLintener listener) {
+        this.listener = listener;
+    }
+
+    /**
+     * 触摸字母的监听器
+     */
+    public interface OnTouchLetterLintener {
+        void onToucheLetter(String letter); //让这个方法传字母出去
+    }
+
 }
